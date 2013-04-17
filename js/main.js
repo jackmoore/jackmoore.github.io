a8f340eb53697cdd3f146b1c9b4bbd67da68d277
Jack Moore <jack@colorpowered.com>
2013-04-17 11:33:31 -0400

    misc

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
 // Analytics
-(function() {
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	window.ga('create', 'UA-40201913-1', 'jacklmoore.com');
-	window.ga('send', 'pageview');
+window.ga('create', 'UA-40201913-1', 'jacklmoore.com');
+window.ga('send', 'pageview');
 
-	// Error logging
-	window.onerror = function(msg, url, line){
-		window.ga('send', 'exception', {
-			'exDescription': url+':'+line+' Error: '+msg+' '
-		});
-	};
-
-	// Download Tracking
-	$(document).on('click', 'a[href$=".zip"]', function(){
-		window.ga('send', 'event', {
-			'eventCategory': 'Downloads',
-			'eventAction': this.href
-		});
+// Error logging
+window.onerror = function(msg, url, line){
+	window.ga('send', 'exception', {
+		'exDescription': url+':'+line+' Error: '+msg+' '
 	});
-})();
+};
+
+// Download Tracking
+$(document).on('click', 'a[href$=".zip"]', function(){
+	window.ga('send', 'event', {
+		'eventCategory': 'Downloads',
+		'eventAction': this.href
+	});
+});
 
 
 if (typeof window.console === 'object' && typeof window.console.log === 'function') {
